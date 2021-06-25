413dba25b036ba15cddad2c400cbb64657726d9e
drime <dhruv.kandula@gmail.com>
2021-06-25 12:47:16 -0700

    add first files

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import pydeck as pdk
+
+# SETTING PAGE CONFIG TO WIDE MODE
+st.set_page_config(layout="wide")
+
+# LOADING DATA
+DATE_TIME = "date/time"
+DATA_URL = (
+    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
+)
+
+st.title("Uber Pickups in NYC")
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    return data
+
+data = load_data(100000)
+hour = st.sidebar.slider('hour', 0, 23, 10, step = 1)
+data = data[data[DATE_TIME].dt.hour == hour]
+
+'Uber Pickup Data at %sh' % hour
+
+st.map(data)
