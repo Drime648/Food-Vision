b78acd3f41d54b3a06cb821195392a4f0064926c
drime <dhruv.kandula@gmail.com>
2021-06-25 18:22:29 -0700

    make the base prediction system

--- app.py
+++ app.py
@@ -4,27 +4,156 @@ import numpy as np
 import altair as alt
 import pydeck as pdk
 import tensorflow as tf
+from PIL import Image
+from torchvision import transforms
 
+
+class_names = ['apple_pie',
+                'baby_back_ribs',
+                'baklava',
+                'beef_carpaccio',
+                'beef_tartare',
+                'beet_salad',
+                'beignets',
+                'bibimbap',
+                'bread_pudding',
+                'breakfast_burrito',
+                'bruschetta',
+                'caesar_salad',
+                'cannoli',
+                'caprese_salad',
+                'carrot_cake',
+                'ceviche',
+                'cheesecake',
+                'cheese_plate',
+                'chicken_curry',
+                'chicken_quesadilla',
+                'chicken_wings',
+                'chocolate_cake',
+                'chocolate_mousse',
+                'churros',
+                'clam_chowder',
+                'club_sandwich',
+                'crab_cakes',
+                'creme_brulee',
+                'croque_madame',
+                'cup_cakes',
+                'deviled_eggs',
+                'donuts',
+                'dumplings',
+                'edamame',
+                'eggs_benedict',
+                'escargots',
+                'falafel',
+                'filet_mignon',
+                'fish_and_chips',
+                'foie_gras',
+                'french_fries',
+                'french_onion_soup',
+                'french_toast',
+                'fried_calamari',
+                'fried_rice',
+                'frozen_yogurt',
+                'garlic_bread',
+                'gnocchi',
+                'greek_salad',
+                'grilled_cheese_sandwich',
+                'grilled_salmon',
+                'guacamole',
+                'gyoza',
+                'hamburger',
+                'hot_and_sour_soup',
+                'hot_dog',
+                'huevos_rancheros',
+                'hummus',
+                'ice_cream',
+                'lasagna',
+                'lobster_bisque',
+                'lobster_roll_sandwich',
+                'macaroni_and_cheese',
+                'macarons',
+                'miso_soup',
+                'mussels',
+                'nachos',
+                'omelette',
+                'onion_rings',
+                'oysters',
+                'pad_thai',
+                'paella',
+                'pancakes',
+                'panna_cotta',
+                'peking_duck',
+                'pho',
+                'pizza',
+                'pork_chop',
+                'poutine',
+                'prime_rib',
+                'pulled_pork_sandwich',
+                'ramen',
+                'ravioli',
+                'red_velvet_cake',
+                'risotto',
+                'samosa',
+                'sashimi',
+                'scallops',
+                'seaweed_salad',
+                'shrimp_and_grits',
+                'spaghetti_bolognese',
+                'spaghetti_carbonara',
+                'spring_rolls',
+                'steak',
+                'strawberry_shortcake',
+                'sushi',
+                'tacos',
+                'takoyaki',
+                'tiramisu',
+                'tuna_tartare',
+                'waffles']
+
+model = tf.keras.models.load_model("food_vision_b4.h5")
+
+def load_and_prep(image, shape=224, scale=False):
+    image = tf.image.decode_image(image, channels=3)
+    image = tf.image.resize(image, size=([shape, shape]))
+    if scale:
+        image = image/255.
+    return image
 
 # SETTING PAGE CONFIG TO WIDE MODE
 st.set_page_config(layout="wide")
+
+
 
 def main():
     """Run this function to display the Streamlit app"""
     # st.info(__doc__)
     # st.markdown(STYLE, unsafe_allow_html=True)
  
-    file = st.file_uploader("Upload file", type=["png", "jpg"])
+    file = st.file_uploader("Upload file", type=["jpg"])
     show_file = st.empty()
  
     if not file:
-        show_file.info("Please upload a file of type: " + "/".join(["png", "jpg"]))
+        show_file.info("Please upload a file of type: " + "/".join(["jpg"]))
         return
  
     content = file.getvalue()
 
     show_file.image(file)
+    if file is not None:
+        image = file.read()
+        image = load_and_prep(image)
+        image = tf.cast(tf.expand_dims(image, axis=0), tf.int32)
+        image.shape
+        pred_probs = model.predict(image)
+        pred = class_names[tf.argmax(pred_probs[0])]
+        pred
+
+
+
     file.close()
+
+
+
  
 main()
 
